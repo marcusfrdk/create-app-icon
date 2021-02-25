6425958f9eb304d6222c4d75c41838792f7c4b53
Marcus Fredriksson <marcus@marcusfredriksson.com>
2021-02-25 23:16:38 +0100

    Fixed reset.py script

--- sizes.py
+++ sizes.py
@@ -10,7 +10,7 @@ sizes = {
         "50x50": False,
         "57x57": False,
         "58x58": False,
-        "60x60": True,
+        "60x60": False,
         "64x64": False,
         "72x72": False,
         "76x76": False,
@@ -21,24 +21,24 @@ sizes = {
         "120x120": False,
         "128x128": False,
         "167x167": False,
-        "144x144": True,
+        "144x144": False,
         "152x152": False,
         "180x180": False,
         "256x256": False,
-        "512x512": True,
+        "512x512": False,
         "1024x1024": False
     },
-    "apple_watch": {
+    "apple-watch": {
         "48x48": False,
         "55x55": False,
         "80x80": False,
-        "88x88": True,
+        "88x88": False,
         "172x172": False,
         "196x196": False
     },
     "android": {
-        "36x36": True,     # LDPI
-        "48x48": True,     # MDPI
+        "36x36": False,     # LDPI
+        "48x48": False,     # MDPI
         "72x72": False,     # HDPI
         "96x96": False,     # XHDPI
         "144x144": False,   # XXHDPI
