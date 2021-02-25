50926b4f99af1d58cecd0ed4c28a3744efa9855a
Marcus Fredriksson <marcus@marcusfredriksson.com>
2021-02-25 23:41:10 +0100

    Fixed reset.py script to allow call from anywhere

--- reset.py
+++ reset.py
@@ -1,12 +1,19 @@
 import shutil, os
+
+# Meta
 file_name = "sizes.py"
+file_path = os.path.abspath(os.path.join(__file__, "..", file_name))
+
 default_file_name = ".default.sizes.py"
+default_file_path = os.path.abspath(os.path.join(__file__, "..", default_file_name))
 
-if os.path.exists(file_name):
-    os.remove(file_name)
+# Current config file
+if os.path.exists(file_path):
+    os.remove(file_path)
 
-if os.path.exists(default_file_name):
-    shutil.copyfile(default_file_name, file_name)
+# Default config file
+if os.path.exists(default_file_path):
+    shutil.copyfile(default_file_path, file_path)
     print("Successfully reset config.")
 else:
     print("Could not reset config, please download it from the git repository.")
